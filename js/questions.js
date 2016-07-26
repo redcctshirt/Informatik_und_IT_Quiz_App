1e5396c4a62deb2e1aaac2378fa0f81f5f186fdd
redcctshirt <info@datenpaul.de>
2016-07-26 18:13:43 +0200

    update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,30 +1,68 @@
 var questions_data = [
 
             {
+            type: "one",
+            tag: "it_grundlagen",
             question: "Was bedeutet die Abkürzung EVA beim Grundschema der Datenverarbeitung namens EVA-Prinzip ?",
             answers: ["Eingabe, Verarbeitung, Ausgabe","Entpackung, Verarbeitung, Algorithmus","ensure, verify, alter","Enders, Versailles, Alkine"],
             correct: 0
             },
             {
+            type: "one",
+            tag: "it_grundlagen",
             question: "Welches Gerät kümmert sich um die Ausgabe in der Datenverarbeitung ?",
             answers: ["CPU","Plotter","Tastatur","Maus"],
             correct: 1
             },
-            { 
+            {
+            type: "one",
+            tag: "it_grundlagen",
             question: "Welches Gerät kümmert sich um die Ausgabe in der Datenverarbeitung ?",
             answers: ["Maus","Barcode-Leser","Drucker","Scanner"],
             correct: 2
             },
-            { 
+            {
+            type: "one",
+            tag: "it_grundlagen",
             question: "Welches Gerät gehört nicht zum Teil der Eingabe in der Datenverarbeitung ?",
             answers: ["Scanner","Tastatur","Maus","Beamer"],
             correct: 3
             },
-            { 
+            {
+            type: "one",
+            tag: "it_grundlagen",
             question: "Bei welchem Gerät sind Ein- und Ausgabe (Datenverarbeitung) nur schwer zu trennen ?",
             answers: ["Smartphone","Scanner","Nadeldrucker","Maus"],
             correct: 0
-            }      
+            },
+            {
+            type: "one",
+            tag: "python",
+            question: "Was ist Python nicht ?",
+            answers: ["eine Programmiersprache","ein Betriebssystem","ein Programm","ein Interpreter"],
+            correct: 1
+            },
+            {
+            type: "one",
+            tag: "python",
+            question: "Mit welcher Eingabe kann man die interaktive Python Konsole verlassen ?",
+            answers: ["ende","end()","exit()","exit"],
+            correct: 2
+            },
+            {
+            type: "one",
+            tag: "git",
+            question: "Was ist Git ?",
+            answers: ["eine objektorientierte Programmiersprache","eine lokale Versionsverwaltung","ein Betriebssystem für mobile Endgeräte","eine verteilte Versionsverwaltung"],
+            correct: 3
+            },
+            {
+            type: "one",
+            tag: "git",
+            question: "Durch wen wurde das Projekt 'Git' angestoßen ?",
+            answers: ["Linus Torvalds","Cecilia Payne","Marie Curie","Guido van Rossum"],
+            correct: 0
+            }
 
 ];
 
